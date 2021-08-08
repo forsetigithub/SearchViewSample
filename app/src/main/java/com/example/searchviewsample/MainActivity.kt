f1e1a29cefba1098bbe8cd24e99bb23803009edd
kimuramasanori <kimura.m.yuko@gmail.com>
2021-08-08 21:51:39 +0900

    second commit

--- app/src/main/java/com/example/searchviewsample/MainActivity.kt
+++ app/src/main/java/com/example/searchviewsample/MainActivity.kt
@@ -2,15 +2,91 @@ package com.example.searchviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.SearchView
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+import com.example.searchviewsample.R
+import com.example.searchviewsample.utils.getQueryTextChangeStateFlow
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    private lateinit var job: Job
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        job = Job()
+        setUpSearchStateFlow()
 
+
+//
+//        val searcher = findViewById<SearchView>(R.id.search)
+//
+//        searcher.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+//            override fun onQueryTextChange(p0: String?): Boolean {
+//                return false
+//            }
+//
+//            override fun onQueryTextSubmit(p0: String?): Boolean {
+//                adapter.filter.filter(p0)
+//                return false
+//            }
+//        })
+    }
+
+    private fun setUpSearchStateFlow() {
+
+        launch {
+            search.getQueryTextChangeStateFlow()
+                .debounce(300)
+                .filter { query ->
+                    return@filter !query.isEmpty()
+                }.distinctUntilChanged()
+                .flatMapConcat { query ->
+                    getDataFromText(query)
+                        .catch {
+                            emitAll(flowOf(""))
+                        }
+                }
+                .flowOn(Dispatchers.Default)
+                .collect { result ->
+                    var adapter = ArrayAdapter (
+                        this@MainActivity,
+                        android.R.layout.simple_list_item_1,
+                        ereaList.filter { s -> s.contains(result) }
+                    )
+                    Log.i("getQueryTextChangeStateFlow",
+                        ereaList.filter { s -> s.contains(result) }.count().toString())
+                    listView.adapter = adapter
+
+                }
+        }
+    }
+
+
+    private fun getDataFromText(query: String): Flow<String> {
+        return flow {
+            kotlinx.coroutines.delay(500)
+            emit(query)
+        }
+    }
+
+    companion object {
         val ereaList = listOf<String>(
             "北海道",
             "青森県",
@@ -60,26 +136,5 @@ class MainActivity : AppCompatActivity() {
             "鹿児島県",
             "沖縄県"
         )
-
-        val adapter = ArrayAdapter (
-            this,
-            android.R.layout.simple_list_item_1,
-            ereaList
-        )
-
-        val listView = findViewById<ListView>(R.id.listView)
-        listView.adapter = adapter
-
-        val search = findViewById<SearchView>(R.id.search)
-        search.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(p0: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextSubmit(p0: String?): Boolean {
-                adapter.filter.filter(p0)
-                return false
-            }
-        })
     }
 }
